58e257fb592a595b90c3acc348c715e1918c81bb
mirhan-aksoy <mirhanaksoy63@gmail.com>
2023-02-25 19:42:33 +0300

    v6

--- src/main/java/Deneme2.java
+++ src/main/java/Deneme2.java
@@ -2,6 +2,7 @@ public class Deneme2 {
     public static void main(String[] args) {
         System.out.println("v1 merhaba");
         System.out.println(" dünya");
+        System.out.println("merhaba dünya");
 
     }
 }
